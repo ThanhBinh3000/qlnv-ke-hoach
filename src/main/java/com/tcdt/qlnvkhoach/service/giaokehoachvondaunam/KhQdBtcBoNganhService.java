e25ed84f1b337f038d8a4e2eb14739bd005f72e2
Binh <binhk4a@gmail.com>
2022-07-14 10:15:16 +0700

    feat BTC BO NGANH

--- src/main/java/com/tcdt/qlnvkhoach/service/giaokehoachvondaunam/KhQdBtcBoNganhService.java
+++ src/main/java/com/tcdt/qlnvkhoach/service/giaokehoachvondaunam/KhQdBtcBoNganhService.java
@@ -1,9 +1,132 @@
 package com.tcdt.qlnvkhoach.service.giaokehoachvondaunam;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tcdt.qlnvkhoach.repository.kehoachquyetdinhbotaichinhbonganh.KhQdBtcBoNganhCtietRepository;
+import com.tcdt.qlnvkhoach.repository.kehoachquyetdinhbotaichinhbonganh.KhQdBtcBoNganhRepository;
+import com.tcdt.qlnvkhoach.request.object.chitieukehoachnam.KhQdBtcBoNganhCtietReq;
+import com.tcdt.qlnvkhoach.request.object.chitieukehoachnam.KhQdBtcBoNganhReq;
+import com.tcdt.qlnvkhoach.request.search.catalog.giaokehoachdaunam.KhQdBtBoNganhSearchReq;
+import com.tcdt.qlnvkhoach.response.giaokehoachvondaunam.KhQdBtcBoNganhCtietRes;
+import com.tcdt.qlnvkhoach.response.giaokehoachvondaunam.KhQdBtcBoNganhRes;
+import com.tcdt.qlnvkhoach.service.SecurityContextService;
+import com.tcdt.qlnvkhoach.table.UserInfo;
+import com.tcdt.qlnvkhoach.table.btcgiaocacbonganh.KhQdBtcBoNganh;
+import com.tcdt.qlnvkhoach.table.btcgiaocacbonganh.KhQdBtcBoNganhCtiet;
+import com.tcdt.qlnvkhoach.util.Contains;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class KhQdBtcBoNganhService {
+    @Autowired
+    private KhQdBtcBoNganhRepository khQdBtcBoNganhRepository;
 
+    @Autowired
+    private KhQdBtcBoNganhCtietRepository khQdBtcBoNganhCtietRepository;
+
+    public Iterable<KhQdBtcBoNganh> findAll() {
+        return khQdBtcBoNganhRepository.findAll();
+    }
+
+    public Page<KhQdBtcBoNganh> searchPage(KhQdBtBoNganhSearchReq objReq) throws Exception{
+        Pageable pageable= PageRequest.of(objReq.getPaggingReq().getPage(),objReq.getPaggingReq().getLimit(), Sort.by("id").ascending());
+        Page<KhQdBtcBoNganh> data=khQdBtcBoNganhRepository.selectPage(
+                objReq.getNamKhoach(),
+                objReq.getSoQd(),
+                Contains.convertDateToString(objReq.getNgayQdTu()),
+                Contains.convertDateToString(objReq.getNgayQdDen()),
+                objReq.getTrichYeu(),
+                pageable);
+        return data;
+    }
+
+    @Transactional
+    public KhQdBtcBoNganh save (KhQdBtcBoNganhRes objReq, HttpServletRequest req) throws Exception{
+        UserInfo userInfo = SecurityContextService.getUser();
+        if (userInfo == null)
+            throw  new Exception("Bad request.");
+        KhQdBtcBoNganh data = new ModelMapper().map(objReq,KhQdBtcBoNganh.class);
+        data.setNgayTao(new Date());
+        data.setNguoiTao(userInfo.getUsername());
+        KhQdBtcBoNganh createCheck = khQdBtcBoNganhRepository.save(data);
+        for(KhQdBtcBoNganhCtietRes bNganhReq : objReq.getListBoNganh()){
+            KhQdBtcBoNganhCtiet cTiet = new ModelMapper().map(bNganhReq,KhQdBtcBoNganhCtiet.class);
+            cTiet.setIdQdBtcNganh(data.getId());
+            khQdBtcBoNganhCtietRepository.save(cTiet);
+        }
+        return createCheck;
+    }
+
+    public <T> void updateObjectToObject(T source, T objectEdit) throws JsonMappingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat(Contains.FORMAT_DATE_STR));
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.updateValue(source, objectEdit);
+    }
+    @Transactional
+    public KhQdBtcBoNganh update(@Valid KhQdBtcBoNganhReq objReq) throws Exception{
+        UserInfo userInfo = SecurityContextService.getUser();
+        if (userInfo == null)
+            throw new Exception("Bad request.");
+        Optional<KhQdBtcBoNganh> qOptional=khQdBtcBoNganhRepository.findById(objReq.getId());
+        if (!qOptional.isPresent())
+            throw new UnsupportedOperationException("Id không tồn tại");
+        KhQdBtcBoNganh data=qOptional.get();
+        KhQdBtcBoNganh dataMap=new ModelMapper().map(objReq,KhQdBtcBoNganh.class);
+        updateObjectToObject(data,dataMap);
+        data.setNguoiSua(userInfo.getUsername());
+        data.setNgaySua(new Date());
+        KhQdBtcBoNganh createCheck=khQdBtcBoNganhRepository.save(data);
+        khQdBtcBoNganhCtietRepository.deleteAllByIdQdBtcNganh(data.getId());
+        for (KhQdBtcBoNganhCtietReq bnCtiet : objReq.getListBoNganh()){
+            KhQdBtcBoNganhCtiet cTiet = new ModelMapper().map(bnCtiet,KhQdBtcBoNganhCtiet.class);
+            cTiet.setId(null);
+            cTiet.setIdQdBtcNganh(data.getId());
+            khQdBtcBoNganhCtietRepository.save(cTiet);
+        }
+        return createCheck;
+    }
+
+    @Transactional
+    public KhQdBtcBoNganh detailBtc(String id) throws  Exception {
+        Optional<KhQdBtcBoNganh> qOptional = khQdBtcBoNganhRepository.findById(Long.parseLong(id));
+        if (!qOptional.isPresent()) {
+            throw new Exception("Kế hoạch quyết định bộ tài chính giao bộ ngành không tồn tại");
+        }
+        KhQdBtcBoNganh data = qOptional.get();
+        List<KhQdBtcBoNganhCtiet> listBoNganh = khQdBtcBoNganhCtietRepository.findAllByIdQdBtcNganh(data.getId());
+        data.setListBoNganh(listBoNganh);
+        return data;
+
+    }
+
+    @Transactional
+    public void delete(Long ids){
+        Optional<KhQdBtcBoNganh> qOptional=khQdBtcBoNganhRepository.findById(ids);
+        if (!qOptional.isPresent()){
+            throw new UnsupportedOperationException("ID không tồn tại");
+        }
+        for (KhQdBtcBoNganhCtiet bNganh : khQdBtcBoNganhCtietRepository.findAllByIdQdBtcNganh(ids)){
+            khQdBtcBoNganhCtietRepository.deleteAllByIdQdBtcNganh(bNganh.getId());
+        }
+        khQdBtcBoNganhRepository.delete(qOptional.get());
+    }
 
 }
