63f1977f4ef60e70018620dfdb2597521fd10cc3
VuNT <vunt@ttkd6.com>
2022-07-20 15:30:50 +0700

    feat quyet dinh

--- src/main/java/com/tcdt/qlnvkhoach/repository/giaokehoachvondaunam/KhQdTtcpRepository.java
+++ src/main/java/com/tcdt/qlnvkhoach/repository/giaokehoachvondaunam/KhQdTtcpRepository.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @Repository
 public interface KhQdTtcpRepository extends CrudRepository<KhQdTtcp ,Long> {
 
-    @Query(value ="SELECT * FROM KH_QD_TTCP QD WHERE (:namQd IS NULL OR QD.NAM = TO_NUMBER(:namQd))"
+    @Query(value ="SELECT * FROM KH_QD_TTCP QD WHERE (:namQd IS NULL OR QD.NAM_QD = TO_NUMBER(:namQd))"
             +"AND (:soQd IS NULL OR LOWER(QD.SO_QD) LIKE LOWER(CONCAT(CONCAT('%',:soQd),'%' ) ) )"
             +"AND (:ngayQdTu IS NULL OR QD.NGAY_QD >=  TO_DATE(:ngayQdTu,'yyyy-MM-dd'))"
             +"AND (:ngayQdDen IS NULL OR QD.NGAY_QD <= TO_DATE(:ngayQdDen,'yyyy-MM-dd'))"
