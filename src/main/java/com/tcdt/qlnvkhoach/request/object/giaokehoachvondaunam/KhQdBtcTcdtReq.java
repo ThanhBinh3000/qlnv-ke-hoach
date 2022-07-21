3f81da34ec5f56254ff5845ab338cafa495ce4d3
thongtv <thongtv@tecapro.com>
2022-07-21 18:33:20 +0700

    api create update detail data detail

--- src/main/java/com/tcdt/qlnvkhoach/request/object/giaokehoachvondaunam/KhQdBtcTcdtReq.java
+++ src/main/java/com/tcdt/qlnvkhoach/request/object/giaokehoachvondaunam/KhQdBtcTcdtReq.java
@@ -1,8 +1,12 @@
 package com.tcdt.qlnvkhoach.request.object.giaokehoachvondaunam;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tcdt.qlnvkhoach.table.btcgiaotcdt.KhQdBtcTcdtCtiet;
+import com.tcdt.qlnvkhoach.util.Contains;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -18,6 +22,7 @@ public class KhQdBtcTcdtReq {
     @Size(max = 20,message = "Số quyết định không được vượt quá 50 ký tự")
     String soQd;
     @NotNull(message = "Không được để trống")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Contains.FORMAT_DATE_STR)
     Date ngayQd;
     @NotNull(message = "Không được để trống")
     @Size(max = 255,message = "Trích Yếu không được vượt quá 255 ký tự")
@@ -25,5 +30,12 @@ public class KhQdBtcTcdtReq {
 
     private List<KhQdBtcTcdtCtietReq> listCtiet;
 
+    private List<KhQdBtcTcdtCtietReq> muaTangList;
+
+    private List<KhQdBtcTcdtCtietReq> xuatGiamList;
+
+    private List<KhQdBtcTcdtCtietReq> xuatBanList;
+
+    private List<KhQdBtcTcdtCtietReq> luanPhienList;
 
 }
