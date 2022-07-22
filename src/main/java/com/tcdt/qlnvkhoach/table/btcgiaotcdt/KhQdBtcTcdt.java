e99bb0bd7bbf7bb4d0f8285043e791e4fd4f3dd9
thongtv <thongtv@tecapro.com>
2022-07-22 16:56:07 +0700

    fig bug màn btc giao bnganh

--- src/main/java/com/tcdt/qlnvkhoach/table/btcgiaotcdt/KhQdBtcTcdt.java
+++ src/main/java/com/tcdt/qlnvkhoach/table/btcgiaotcdt/KhQdBtcTcdt.java
@@ -16,7 +16,7 @@ public class KhQdBtcTcdt implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "KH_QD_BTC_TCDT_SEQ")
     @SequenceGenerator(sequenceName = "KH_QD_BTC_TCDT_SEQ",allocationSize = 1,name = "KH_QD_BTC_TCDT_SEQ")
     private Long id;
-    Integer namKhoach;
+    Integer namQd;
     String soQd;
     @Temporal(TemporalType.DATE)
     Date ngayQd;
