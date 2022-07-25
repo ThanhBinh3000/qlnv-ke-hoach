3c74fba66b0fc38982912ac78c6beace339fad33
thongtv <thongtv@tecapro.com>
2022-07-25 17:25:45 +0700

    full api màn quyết định UBTVQH

---
+++ src/main/java/com/tcdt/qlnvkhoach/request/search/catalog/giaokehoachvonmuabubosung/KhMuaQdUbtvqhSearchReq.java
@@ -0,0 +1,27 @@
+package com.tcdt.qlnvkhoach.request.search.catalog.giaokehoachvonmuabubosung;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.tcdt.qlnvkhoach.request.BaseRequest;
+import com.tcdt.qlnvkhoach.util.Contains;
+import lombok.Data;
+import java.util.Date;
+import java.util.List;
+@Data
+public class KhMuaQdUbtvqhSearchReq extends BaseRequest {
+
+    Integer namQd;
+
+    String soQd;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Contains.FORMAT_DATE_STR)
+    Date ngayQdTu;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Contains.FORMAT_DATE_STR)
+    Date ngayQdDen;
+
+    String trichYeu;
+
+    String trangThai;
+
+    List<Long> idList;
+}
