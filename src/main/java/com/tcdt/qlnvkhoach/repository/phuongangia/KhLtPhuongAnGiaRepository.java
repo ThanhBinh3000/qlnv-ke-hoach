5676debe5128084e1d2ccf94cede6dee3d64132d
othoa <othoa>
2022-08-17 09:01:30 +0700

    update de xuat pag

--- src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhLtPhuongAnGiaRepository.java
+++ src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhLtPhuongAnGiaRepository.java
@@ -17,7 +17,7 @@ public interface KhLtPhuongAnGiaRepository extends JpaRepository<KhLtPhuongAnGia
 
 	@Query(value ="SELECT * FROM KH_LT_PHUONG_AN_GIA KLPAG WHERE (:namKh IS NULL OR KLPAG.NAM_KE_HOACH = TO_NUMBER(:namKh))"
 			+"AND (:soDx IS NULL OR LOWER(KLPAG.SO_DE_XUAT) LIKE LOWER(CONCAT(CONCAT('%',:soDx),'%' ) ) )"
-			+"AND (:loaiHh IS NULL OR KLPAG.LOAI_HANG_HOA =  :loaiHh)"
+			+"AND (:loaiHh IS NULL OR KLPAG.LOAI_VTHH =  :loaiHh)"
 			+"AND (:ngayKyTu IS NULL OR KLPAG.NGAY_KY >=  TO_DATE(:ngayKyTu,'yyyy-MM-dd'))"
 			+"AND (:ngayKyDen IS NULL OR KLPAG.NGAY_KY <= TO_DATE(:ngayKyDen,'yyyy-MM-dd'))"
 			+"AND (:trichYeu IS NULL  OR LOWER(KLPAG.TRICH_YEU) LIKE LOWER(CONCAT(CONCAT('%',:trichYeu),'%' ) ) )"
