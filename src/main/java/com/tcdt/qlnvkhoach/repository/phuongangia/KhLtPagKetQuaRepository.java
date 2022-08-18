8ffcca3e5baa7524e51a38f4929adf969d119036
othoa <othoa>
2022-08-18 13:43:56 +0700

    change field name Ma_to_trinh to so_to_trinh

--- src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhLtPagKetQuaRepository.java
+++ src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhLtPagKetQuaRepository.java
@@ -13,6 +13,8 @@ public interface KhLtPagKetQuaRepository extends JpaRepository<KhPagKetQua, Long
 	List<KhPagKetQua> findByTypeAndIdIn(String type, List<Long> ids);
 	List<KhPagKetQua> findByTypeAndPhuongAnGiaIdIn(String type, List<Long> ids);
 
+	List<KhPagKetQua> findByPhuongAnGiaIdIn(List<Long> ids);
+
 	@Query("SELECT kq.id,kq.stt,kq.tenDviBaoGia,kq.donGia,kq.donGiaVat,kq.cloaiVthh,kq.ghiChu,kq.thoiHanBaoGia,kq.phuongAnGiaId,kq.type FROM KhPagKetQua kq " +
 			"INNER JOIN KhPhuongAnGia pag ON kq.phuongAnGiaId = pag.id " +
 			"WHERE kq.type = ?1 and kq.phuongAnGiaId IN ?2")
