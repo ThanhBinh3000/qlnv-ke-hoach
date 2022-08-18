f1766fc91cb055551328a81927bc166ff2d3fac2
othoa <othoa>
2022-08-18 18:32:02 +0700

    change field name Ma_to_trinh to so_to_trinh

--- src/main/java/com/tcdt/qlnvkhoach/service/phuongangia/KhLtPagService.java
+++ src/main/java/com/tcdt/qlnvkhoach/service/phuongangia/KhLtPagService.java
@@ -73,11 +73,11 @@ public class KhLtPagService {
         return khLtPhuongAnGiaRepository.findAll();
     }
 
-    public Page<KhPhuongAnGia> searchPage(KhLtPhuongAnGiaSearchReq objReq) throws Exception{
+    public Page<KhPhuongAnGia> searchPage(KhLtPhuongAnGiaSearchReq objReq) throws Exception {
         UserInfo userInfo = SecurityContextService.getUser();
-        Pageable pageable= PageRequest.of(objReq.getPaggingReq().getPage(),
+        Pageable pageable = PageRequest.of(objReq.getPaggingReq().getPage(),
                 objReq.getPaggingReq().getLimit(), Sort.by("id").ascending());
-        Page<KhPhuongAnGia> data =khLtPhuongAnGiaRepository.selectPage(
+        Page<KhPhuongAnGia> data = khLtPhuongAnGiaRepository.selectPage(
                 objReq.getNamKh(),
                 objReq.getSoDx(),
                 objReq.getLoaiHh(),
@@ -89,21 +89,21 @@ public class KhLtPagService {
                 objReq.getPagType().equals("VT") ? "02" : "01",
                 pageable);
 
-        Map<String,String> hashMapHh = qlnvDmService.getListDanhMucHangHoa();
-        Map<String,String> hashMapLoaiGia = qlnvDmService.getListDanhMucChung("LOAI_GIA");
+        Map<String, String> hashMapHh = qlnvDmService.getListDanhMucHangHoa();
+        Map<String, String> hashMapLoaiGia = qlnvDmService.getListDanhMucChung("LOAI_GIA");
 
-        data.getContent().forEach( f -> {
+        data.getContent().forEach(f -> {
             f.setTenTrangThai(PAGTrangThaiEnum.getTrangThaiDuyetById(f.getTrangThai()));
             f.setTenTrangThaiTh(Contains.getThTongHop(f.getTrangThaiTh()));
             f.setTenLoaiVthh(StringUtils.isEmpty(f.getLoaiVthh()) ? null : hashMapHh.get(f.getLoaiVthh()));
-            f.setTenLoaiGia(StringUtils.isEmpty(f.getLoaiGia()) ? null :  hashMapLoaiGia.get(f.getLoaiGia()));
+            f.setTenLoaiGia(StringUtils.isEmpty(f.getLoaiGia()) ? null : hashMapLoaiGia.get(f.getLoaiGia()));
             f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
         });
         List<Long> khLtPagIds = data.getContent().stream().map(KhPhuongAnGia::getId).collect(Collectors.toList());
 //        get ketqua tham dinh gia va khao sat gia
-        Map<Long, List<Object[]>> khLtKetquaThamDinhs = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdsIn(PhuongAnGiaEnum.KET_QUA_THAM_DINH_GIA.getValue(),khLtPagIds)
+        Map<Long, List<Object[]>> khLtKetquaThamDinhs = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdsIn(PhuongAnGiaEnum.KET_QUA_THAM_DINH_GIA.getValue(), khLtPagIds)
                 .stream().collect(Collectors.groupingBy(o -> (Long) o[8]));
-        Map<Long, List<Object[]>> khLtKetquaKhaoSats = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdsIn(PhuongAnGiaEnum.KET_QUA_KHAO_SAT_GIA_THI_TRUONG.getValue(),khLtPagIds)
+        Map<Long, List<Object[]>> khLtKetquaKhaoSats = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdsIn(PhuongAnGiaEnum.KET_QUA_KHAO_SAT_GIA_THI_TRUONG.getValue(), khLtPagIds)
                 .stream().collect(Collectors.groupingBy(o -> (Long) o[8]));
         Map<Long, List<Object[]>> khLtCCPhapLys = khPagCcPhapLyRepository.findByPhuongAnGiaIdsIn(khLtPagIds)
                 .stream().collect(Collectors.groupingBy(o -> (Long) o[3]));
@@ -114,19 +114,19 @@ public class KhLtPagService {
             List<KhPagKetQua> khPagKetQuaThamDinhs = new ArrayList<>();
             if (!CollectionUtils.isEmpty(ketquaTDs)) {
                 ketquaTDs.forEach(c -> {
-                    khPagKetQuaThamDinhs.add(new KhPagKetQua((Long) c[0], (Long) c[1], (String) c[2], (BigDecimal) c[3],(BigDecimal) c[4], (String) c[5], (String) c[6], (String) c[7], null, (String) c[9], (Long) c[8],null));
+                    khPagKetQuaThamDinhs.add(new KhPagKetQua((Long) c[0], (Long) c[1], (String) c[2], (BigDecimal) c[3], (BigDecimal) c[4], (String) c[5], (String) c[6], (String) c[7], null, (String) c[9], (Long) c[8], null));
                 });
             }
             List<KhPagKetQua> khPagKetQuaKhaoSats = new ArrayList<>();
             if (!CollectionUtils.isEmpty(ketquaKSs)) {
                 ketquaKSs.forEach(c -> {
-                    khPagKetQuaKhaoSats.add(new KhPagKetQua((Long) c[0], (Long) c[1], (String) c[2], (BigDecimal) c[3],(BigDecimal) c[4], (String) c[5], (String) c[6], (String) c[7], null, (String) c[9], (Long) c[8],null));
+                    khPagKetQuaKhaoSats.add(new KhPagKetQua((Long) c[0], (Long) c[1], (String) c[2], (BigDecimal) c[3], (BigDecimal) c[4], (String) c[5], (String) c[6], (String) c[7], null, (String) c[9], (Long) c[8], null));
                 });
             }
             List<KhPagCcPhapLy> khPagCcPhapLIES = new ArrayList<>();
             if (!CollectionUtils.isEmpty(ccPhapLys)) {
                 ccPhapLys.forEach(c -> {
-                    khPagCcPhapLIES.add(new KhPagCcPhapLy((Long)c[0], (Long) c[1],(String) c[2],null,(Long) c[3]));
+                    khPagCcPhapLIES.add(new KhPagCcPhapLy((Long) c[0], (Long) c[1], (String) c[2], null, (Long) c[3]));
                 });
             }
             khPhuongAnGia.setKetQuaThamDinhGia(khPagKetQuaThamDinhs);
@@ -141,10 +141,10 @@ public class KhLtPagService {
         UserInfo userInfo = SecurityContextService.getUser();
         if (userInfo == null) throw new Exception("Bad request.");
         log.info("Save: thông tin phương án giá");
-        if(khLtPhuongAnGiaRepository.findBySoDeXuat(req.getSoDeXuat()).isPresent()){
+        if (khLtPhuongAnGiaRepository.findBySoDeXuat(req.getSoDeXuat()).isPresent()) {
             throw new Exception("Số đề xuất đã tồn tại trong hệ thống!");
         }
-        KhPhuongAnGia phuongAnGia =  mapper.map(req, KhPhuongAnGia.class);
+        KhPhuongAnGia phuongAnGia = mapper.map(req, KhPhuongAnGia.class);
         phuongAnGia.setTrangThai(PAGTrangThaiEnum.DU_THAO.getId());
         phuongAnGia.setTrangThaiTh(PAGTrangThaiTHEnum.CHUA_TH.getId());
         phuongAnGia.setMaDvi(userInfo.getDvql());
@@ -219,7 +219,7 @@ public class KhLtPagService {
     @Transactional(rollbackFor = Exception.class)
     public KhLtPhuongAnGiaRes update(KhLtPhuongAnGiaReq req) throws Exception {
         if (req == null) return null;
-        if(req.getId() == null ){
+        if (req.getId() == null) {
             throw new Exception("Bad request");
         }
         UserInfo userInfo = SecurityContextService.getUser();
@@ -228,7 +228,7 @@ public class KhLtPagService {
         Optional<KhPhuongAnGia> optional = khLtPhuongAnGiaRepository.findById(req.getId());
         if (!optional.isPresent()) throw new Exception("Đề xuất phương án giá không tồn tại");
         Optional<KhPhuongAnGia> optionalCheckUnique = khLtPhuongAnGiaRepository.findBySoDeXuat(req.getSoDeXuat());
-        if(optionalCheckUnique.isPresent() && req.getId() != optionalCheckUnique.get().getId() ){
+        if (optionalCheckUnique.isPresent() && req.getId() != optionalCheckUnique.get().getId()) {
             throw new Exception("Số đề xuất đã tồn tại");
         }
         KhPhuongAnGia phuongAnGia = optional.get();
@@ -308,7 +308,7 @@ public class KhLtPagService {
         return true;
     }
 
-    private void deleteChildOfDxPag(List<Long> pagIds){
+    private void deleteChildOfDxPag(List<Long> pagIds) {
         log.info("Xóa căn cứ pháp lý và file đính kèm");
         List<KhPagCcPhapLy> khPagCcPhapLyList = khPagCcPhapLyRepository.findByPhuongAnGiaIdIn(pagIds);
         if (!CollectionUtils.isEmpty(khPagCcPhapLyList)) {
@@ -326,7 +326,6 @@ public class KhLtPagService {
     }
 
 
-
     private void deleteKetQua(String type, List<Long> phuongAnGiaIds) {
         List<KhPagKetQua> ketQuaList = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(type, phuongAnGiaIds);
         if (CollectionUtils.isEmpty(ketQuaList)) return;
@@ -337,9 +336,9 @@ public class KhLtPagService {
 
 
     @javax.transaction.Transactional
-    public void delete(Long ids) throws Exception{
-        Optional<KhPhuongAnGia> qOptional=khLtPhuongAnGiaRepository.findById(ids);
-        if(!qOptional.isPresent()){
+    public void delete(Long ids) throws Exception {
+        Optional<KhPhuongAnGia> qOptional = khLtPhuongAnGiaRepository.findById(ids);
+        if (!qOptional.isPresent()) {
             throw new UserPrincipalNotFoundException("Id không tồn tại");
         }
         List<Long> pagIds = new ArrayList<>();
@@ -359,94 +358,98 @@ public class KhLtPagService {
     }
 
     @javax.transaction.Transactional
-    public KhPhuongAnGia detailDxPag(String id) throws  Exception {
+    public KhPhuongAnGia detailDxPag(String id) throws Exception {
         Optional<KhPhuongAnGia> qOptional = khLtPhuongAnGiaRepository.findById(Long.parseLong(id));
         if (!qOptional.isPresent()) {
             throw new Exception("Đề xuất phương án giá không tồn tại");
         }
         KhPhuongAnGia data = qOptional.get();
-        List<Long> ids = new ArrayList<>();
-        ids.add(data.getId());
-        Collection dataType = new ArrayList();
-        dataType.add(KhPhuongAnGia.TABLE_NAME);
-        List<FileDinhKemChung> fileDinhKems = fileDinhKemChungRepository.findByDataIdAndDataTypeIn(data.getId(),dataType);
-        Map<String,String> hashMapHh = qlnvDmService.getListDanhMucHangHoa();
-        List<KhPagDiaDiemDeHang> diaDiemDeHangs = khLtPagDiaDiemDeHangRepository.findByPagIdIn(ids);
-        List<KhPagCcPhapLy> listPagCCPhapLy = khPagCcPhapLyRepository.findByPhuongAnGiaIdIn(ids);
-        List<KhPagKetQua> listPagKetQuaTD = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.KET_QUA_THAM_DINH_GIA.getValue(),ids);
-        List<KhPagKetQua> listPagKetQuaKSTT = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.KET_QUA_KHAO_SAT_GIA_THI_TRUONG.getValue(),ids);
-        List<KhPagKetQua> listPagKetQuaTTHHTT = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.THONG_TIN_GIA_CUA_HANG_HOA_TUONG_TU.getValue(),ids);
-        List<Long> khCcPhapLyIds = listPagCCPhapLy.stream().map(KhPagCcPhapLy::getId).collect(Collectors.toList());
+        try {
+            List<Long> ids = new ArrayList<>();
+            ids.add(data.getId());
+            Collection dataType = new ArrayList();
+            dataType.add(KhPhuongAnGia.TABLE_NAME);
+            List<FileDinhKemChung> fileDinhKems = fileDinhKemChungRepository.findByDataIdAndDataTypeIn(data.getId(), dataType);
+            Map<String, String> hashMapHh = qlnvDmService.getListDanhMucHangHoa();
+            List<KhPagDiaDiemDeHang> diaDiemDeHangs = khLtPagDiaDiemDeHangRepository.findByPagIdIn(ids);
+            List<KhPagCcPhapLy> listPagCCPhapLy = khPagCcPhapLyRepository.findByPhuongAnGiaIdIn(ids);
+            List<KhPagKetQua> listPagKetQuaTD = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.KET_QUA_THAM_DINH_GIA.getValue(), ids);
+            List<KhPagKetQua> listPagKetQuaKSTT = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.KET_QUA_KHAO_SAT_GIA_THI_TRUONG.getValue(), ids);
+            List<KhPagKetQua> listPagKetQuaTTHHTT = khLtPagKetQuaRepository.findByTypeAndPhuongAnGiaIdIn(PhuongAnGiaEnum.THONG_TIN_GIA_CUA_HANG_HOA_TUONG_TU.getValue(), ids);
+            List<Long> khCcPhapLyIds = listPagCCPhapLy.stream().map(KhPagCcPhapLy::getId).collect(Collectors.toList());
 
-        List<Long> listKqIds = khLtPagKetQuaRepository.findByPhuongAnGiaIdIn(ids).stream().map(KhPagKetQua::getId).collect(Collectors.toList());
+            List<Long> listKqIds = khLtPagKetQuaRepository.findByPhuongAnGiaIdIn(ids).stream().map(KhPagKetQua::getId).collect(Collectors.toList());
 
-        Map<Long, List<FileDinhKemChung>> mapListFileForKqs = fileDinhKemChungRepository.findByDataIdInAndDataType(listKqIds,KhPagKetQua.TABLE_NAME)
-                .stream().collect(Collectors.groupingBy(o ->o.getDataId()));
-        if(listPagCCPhapLy.size() > 0){
-            Map<Long, List<FileDinhKemChung>> mapListFile = fileDinhKemChungRepository.findByDataIdInAndDataType(khCcPhapLyIds,KhPagCcPhapLy.TABLE_NAME)
-                    .stream().collect(Collectors.groupingBy(o ->o.getDataId()));
-            listPagCCPhapLy.forEach( f -> {
-                f.setFileDinhKem(mapListFile.size() > 0 && mapListFile.get(f.getId()).size() >0  ? mapListFile.get(f.getId()).get(0) :null);
-            });
-            data.setCanCuPhapLy(listPagCCPhapLy);
-        }
-        if(listPagKetQuaTD.size() > 0){
-            listPagKetQuaTD.forEach( f -> {
-                f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
-                f.setFileDinhKem(mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()).size() >0  ? mapListFileForKqs.get(f.getId()).get(0) :null);
-            });
-            data.setKetQuaThamDinhGia(listPagKetQuaTD);
-        }
-        if(listPagKetQuaKSTT.size() > 0){
-            listPagKetQuaKSTT.forEach( f -> {
-                f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
-                f.setFileDinhKem(mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()).size() >0  ? mapListFileForKqs.get(f.getId()).get(0) :null);
-            });
-            data.setKetQuaKhaoSatGiaThiTruong(listPagKetQuaKSTT);
-        }
-        if(listPagKetQuaTTHHTT.size() > 0){
-            listPagKetQuaTTHHTT.forEach( f -> {
-                f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
-                f.setFileDinhKem(mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()).size() >0  ? mapListFileForKqs.get(f.getId()).get(0) :null);
-            });
-            data.setThongTinGiaHangHoaTuongTu(listPagKetQuaTTHHTT);
-        }
-        if(diaDiemDeHangs.size() > 0){
-            data.setDiaDiemDeHangs(diaDiemDeHangs);
-        }
-        if(fileDinhKems.size() > 0){
-            data.setListFileCCs(fileDinhKems);
+            Map<Long, List<FileDinhKemChung>> mapListFileForKqs = fileDinhKemChungRepository.findByDataIdInAndDataType(listKqIds, KhPagKetQua.TABLE_NAME)
+                    .stream().collect(Collectors.groupingBy(o -> o.getDataId()));
+            if (listPagCCPhapLy.size() > 0) {
+                Map<Long, List<FileDinhKemChung>> mapListFile = fileDinhKemChungRepository.findByDataIdInAndDataType(khCcPhapLyIds, KhPagCcPhapLy.TABLE_NAME)
+                        .stream().collect(Collectors.groupingBy(o -> o.getDataId()));
+                listPagCCPhapLy.forEach(f -> {
+                    f.setFileDinhKem(mapListFile != null && mapListFile.size() > 0 && mapListFile.get(f.getId()).size() > 0 ? mapListFile.get(f.getId()).get(0) : null);
+                });
+                data.setCanCuPhapLy(listPagCCPhapLy);
+            }
+            if (listPagKetQuaTD.size() > 0) {
+                listPagKetQuaTD.forEach(f -> {
+                    f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
+                    f.setFileDinhKem(( mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()) != null && mapListFileForKqs.get(f.getId()).size() > 0 ) ? mapListFileForKqs.get(f.getId()).get(0) : null);
+                });
+                data.setKetQuaThamDinhGia(listPagKetQuaTD);
+            }
+            if (listPagKetQuaKSTT.size() > 0) {
+                listPagKetQuaKSTT.forEach(f -> {
+                    f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
+                    f.setFileDinhKem(( mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()) != null && mapListFileForKqs.get(f.getId()).size() > 0) ? mapListFileForKqs.get(f.getId()).get(0) : null);
+                });
+                data.setKetQuaKhaoSatGiaThiTruong(listPagKetQuaKSTT);
+            }
+            if (listPagKetQuaTTHHTT.size() > 0) {
+                listPagKetQuaTTHHTT.forEach(f -> {
+                    f.setTenCloaiVthh(StringUtils.isEmpty(f.getCloaiVthh()) ? null : hashMapHh.get(f.getCloaiVthh()));
+                    f.setFileDinhKem(( mapListFileForKqs.size() > 0 && mapListFileForKqs.get(f.getId()) != null && mapListFileForKqs.get(f.getId()).size() > 0 ) ? mapListFileForKqs.get(f.getId()).get(0) : null);
+                });
+                data.setThongTinGiaHangHoaTuongTu(listPagKetQuaTTHHTT);
+            }
+            if (diaDiemDeHangs.size() > 0) {
+                data.setDiaDiemDeHangs(diaDiemDeHangs);
+            }
+            if (fileDinhKems.size() > 0) {
+                data.setListFileCCs(fileDinhKems);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return data;
     }
 
-    public  void exportDxPag(KhLtPhuongAnGiaSearchReq objReq, HttpServletResponse response) throws Exception{
+    public void exportDxPag(KhLtPhuongAnGiaSearchReq objReq, HttpServletResponse response) throws Exception {
         PaggingReq paggingReq = new PaggingReq();
         paggingReq.setPage(0);
         paggingReq.setLimit(Integer.MAX_VALUE);
         objReq.setPaggingReq(paggingReq);
-        Page<KhPhuongAnGia> page=this.searchPage(objReq);
-        List<KhPhuongAnGia> data=page.getContent();
+        Page<KhPhuongAnGia> page = this.searchPage(objReq);
+        List<KhPhuongAnGia> data = page.getContent();
 
-        String title="Danh sách đề xuất phương án giá";
-        String[] rowsName=new String[]{"STT","Số đề xuất","Ngày ký","Trích yếu","Năm kế hoạch","Loại hàng hóa","Loại giá","Trạng thái"};
-        String fileName="danh-sach-de-xuat-phuong-an-gia.xlsx";
+        String title = "Danh sách đề xuất phương án giá";
+        String[] rowsName = new String[]{"STT", "Số đề xuất", "Ngày ký", "Trích yếu", "Năm kế hoạch", "Loại hàng hóa", "Loại giá", "Trạng thái"};
+        String fileName = "danh-sach-de-xuat-phuong-an-gia.xlsx";
         List<Object[]> dataList = new ArrayList<Object[]>();
-        Object[] objs=null;
-        for (int i=0;i<data.size();i++){
-            KhPhuongAnGia dx=data.get(i);
-            objs=new Object[rowsName.length];
-            objs[0]=i;
-            objs[1]=dx.getSoDeXuat();
-            objs[2]=dx.getNgayKy();
-            objs[3]=dx.getTrichYeu();
-            objs[4]=dx.getNamKeHoach();
-            objs[5]=dx.getLoaiVthh();
-            objs[6]=dx.getLoaiGia();
-            objs[7]=dx.getTenTrangThai();
+        Object[] objs = null;
+        for (int i = 0; i < data.size(); i++) {
+            KhPhuongAnGia dx = data.get(i);
+            objs = new Object[rowsName.length];
+            objs[0] = i;
+            objs[1] = dx.getSoDeXuat();
+            objs[2] = dx.getNgayKy();
+            objs[3] = dx.getTrichYeu();
+            objs[4] = dx.getNamKeHoach();
+            objs[5] = dx.getLoaiVthh();
+            objs[6] = dx.getLoaiGia();
+            objs[7] = dx.getTenTrangThai();
             dataList.add(objs);
         }
-        ExportExcel ex =new ExportExcel(title,fileName,rowsName,dataList,response);
+        ExportExcel ex = new ExportExcel(title, fileName, rowsName, dataList, response);
         ex.export();
     }
 }
