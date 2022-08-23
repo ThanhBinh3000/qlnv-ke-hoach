8b640f1556a3832cd8b762fa5c5bab4e36337941
thongtv <thongtv@tecapro.com>
2022-08-23 10:37:28 +0700

    màn Phương án giá ->quyết định giá của Tcdtnn

---
+++ src/main/java/com/tcdt/qlnvkhoach/request/phuongangia/KhPagGctQdTcdtnnReq.java
@@ -0,0 +1,67 @@
+package com.tcdt.qlnvkhoach.request.phuongangia;
+
+import com.tcdt.qlnvkhoach.entities.phuongangia.KhPagTongHop;
+import com.tcdt.qlnvkhoach.entities.phuongangia.KhPagTongHopCTiet;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KhPagGctQdTcdtnnReq{
+    private Long id;
+
+    private String maDvi;
+
+    private String trangThai;
+
+    @NotNull(message = "Không được để trống")
+    @ApiModelProperty(example = "2022")
+    private Long namKeHoach;
+
+    @NotNull(message = "Không được để trống")
+    private String soQd;
+
+    private LocalDate ngayKy;
+
+    private LocalDate ngayHieuLuc;
+
+    @NotNull(message = "Không được để trống")
+    private String soToTrinh;
+
+    private String loaiVthh;
+
+    private String cloaiVthh;
+
+    private String loaiGia;
+
+    private String tchuanCluong;
+
+    @NotNull(message = "Không được để trống")
+    private String trichYeu;
+
+    @NotNull(message = "Không được để trống")
+    private String ghiChu;
+
+    private String capDvi;
+
+    private BigDecimal giaMtdbttBtc;
+
+    private BigDecimal giaMtdbttVatBtc;
+
+    private KhPagTongHop soTtDeXuat;
+
+    private List<KhPagTongHopCTiet> thongTinGia;
+
+
+}
