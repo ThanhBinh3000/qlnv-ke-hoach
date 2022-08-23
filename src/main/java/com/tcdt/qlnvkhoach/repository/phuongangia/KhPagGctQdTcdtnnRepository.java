02e27b5220bf1acba05ad2041d680a0ddcfae473
thongtv <thongtv@tecapro.com>
2022-08-23 11:42:57 +0700

    fix bug->qd giá của Tcdtnn

--- src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhPagGctQdTcdtnnRepository.java
+++ src/main/java/com/tcdt/qlnvkhoach/repository/phuongangia/KhPagGctQdTcdtnnRepository.java
@@ -17,9 +17,10 @@ public interface KhPagGctQdTcdtnnRepository extends JpaRepository<KhPagGctQdTcdt
             +" AND (:soQd IS NULL OR LOWER(KLPAG.SO_QD) LIKE LOWER(CONCAT(CONCAT('%',:soQd),'%' ) ) )"
             +" AND (:ngayKyTu IS NULL OR KLPAG.NGAY_KY >=  TO_DATE(:ngayKyTu,'yyyy-MM-dd'))"
             +" AND (:ngayKyDen IS NULL OR KLPAG.NGAY_KY <= TO_DATE(:ngayKyDen,'yyyy-MM-dd'))"
+            +" AND (:pagType IS NULL OR KLPAG.LOAI_VTHH LIKE CONCAT(:pagType,'%' ) )"
             +" AND (:trichYeu IS NULL  OR LOWER(KLPAG.TRICH_YEU) LIKE LOWER(CONCAT(CONCAT('%',:trichYeu),'%' ) ) )"
             , nativeQuery = true)
-    Page<KhPagGctQdTcdtnn> selectPage(Integer namKh, String soQd, String ngayKyTu, String ngayKyDen, String trichYeu , Pageable pageable);
+    Page<KhPagGctQdTcdtnn> selectPage(Integer namKh, String soQd, String ngayKyTu, String ngayKyDen, String trichYeu ,String pagType, Pageable pageable);
 
     Optional<KhPagGctQdTcdtnn> findBySoQd(String soQd);
 
--- src/main/java/com/tcdt/qlnvkhoach/request/search/catalog/phuongangia/KhPagGctQdTcdtnnSearchReq.java
+++ src/main/java/com/tcdt/qlnvkhoach/request/search/catalog/phuongangia/KhPagGctQdTcdtnnSearchReq.java
@@ -21,4 +21,6 @@ public class KhPagGctQdTcdtnnSearchReq extends BaseRequest {
 
     String trichYeu;
 
+    String PagType;
+
 }
