da733c98efcd2d099092b543b2e5b18ecd752e8f
othoa <othoa>
2022-08-29 09:53:44 +0700

    change field name Ma_to_trinh to so_to_trinh

---
+++ src/main/java/com/tcdt/qlnvkhoach/entities/khcnbq/CnCtNcCanCu.java
@@ -0,0 +1,36 @@
+package com.tcdt.qlnvkhoach.entities.khcnbq;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.tcdt.qlnvkhoach.entities.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = CnCtNcCanCu.TABLE_NAME)
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CnCtNcCanCu extends BaseEntity implements Serializable {
+
+    public static final String TABLE_NAME = "CN_CONGTRINH_NGHIENCUU_CANCU";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name="NOI_DUNG")
+    private String noiDung;
+
+    @Column(name="CN_CT_NC_ID")
+    private Long cnCtNcId;
+}
