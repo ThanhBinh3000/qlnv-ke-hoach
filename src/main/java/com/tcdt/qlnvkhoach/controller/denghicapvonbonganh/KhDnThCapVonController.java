ff61924a120798e4b32c4c028d338716be0ac310
DESKTOP-PLEIBBC\hungt <hung.ta@fruitful.io>
2022-09-20 00:16:59 +0700

    Tổng hợp đề nghị cấp vốn DTQG

--- src/main/java/com/tcdt/qlnvkhoach/controller/denghicapvonbonganh/KhDnThCapVonController.java
+++ src/main/java/com/tcdt/qlnvkhoach/controller/denghicapvonbonganh/KhDnThCapVonController.java
@@ -1,7 +1,10 @@
 package com.tcdt.qlnvkhoach.controller.denghicapvonbonganh;
 
 import com.tcdt.qlnvkhoach.enums.EnumResponse;
+import com.tcdt.qlnvkhoach.request.DeleteReq;
+import com.tcdt.qlnvkhoach.request.StatusReq;
 import com.tcdt.qlnvkhoach.request.denghicapvonbonganh.KhDnThCapVonRequest;
+import com.tcdt.qlnvkhoach.request.denghicapvonbonganh.KhDnThCapVonSearchRequest;
 import com.tcdt.qlnvkhoach.response.Resp;
 import com.tcdt.qlnvkhoach.service.denghicapvonbonganh.KhDnThCapVonService;
 import com.tcdt.qlnvkhoach.util.PathConstants;
@@ -10,9 +13,12 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -72,5 +78,83 @@ public class KhDnThCapVonController {
             log.error("Xóa Quản lý Tổng hợp đề nghị cấp vốn DTQG lỗi: {}", e);
         }
         return ResponseEntity.ok(resp);
+    }
+
+    @ApiOperation(value = "Chi tiết Quản lý Tổng hợp đề nghị cấp vốn DTQG", response = List.class)
+    @GetMapping("/{id}")
+    public ResponseEntity<Resp> detail(@PathVariable Long id) {
+        Resp resp = new Resp();
+        try {
+            resp.setData(service.detail(id));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch (Exception e) {
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+            log.error("Chi tiết Quản lý Tổng hợp đề nghị cấp vốn DTQG lỗi: {}", e);
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+    @ApiOperation(value = "Phê duyệt/ từ chối Quản lý Tổng hợp đề nghị cấp vốn DTQG", response = List.class)
+    @PutMapping("/status")
+    public ResponseEntity<Resp> updateStatus(@Valid @RequestBody StatusReq req) {
+        Resp resp = new Resp();
+        try {
+            resp.setData(service.updateStatusQd(req));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch (Exception e) {
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+            log.error("Phê duyệt/ từ chối Quản lý Tổng hợp đề nghị cấp vốn DTQG lỗi: {}", e);
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+    @ApiOperation(value = "Tra cứu Quản lý Tổng hợp đề nghị cấp vốn DTQG", response = List.class)
+    @GetMapping()
+    public ResponseEntity<Resp> search(KhDnThCapVonSearchRequest req) {
+        Resp resp = new Resp();
+        try {
+            resp.setData(service.search(req));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch (Exception e) {
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+            log.error("Tra cứu Quản lý Tổng hợp đề nghị cấp vốn DTQG lỗi: {}", e);
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+    @ApiOperation(value = "Delete multiple Tổng hợp đề nghị cấp vốn DTQG", response = List.class)
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/delete/multiple")
+    public final ResponseEntity<Resp> deleteMultiple(@RequestBody @Valid DeleteReq req) {
+        Resp resp = new Resp();
+        try {
+            resp.setData(service.deleteMultiple(req));
+            resp.setStatusCode(EnumResponse.RESP_SUCC.getValue());
+            resp.setMsg(EnumResponse.RESP_SUCC.getDescription());
+        } catch (Exception e) {
+            resp.setStatusCode(EnumResponse.RESP_FAIL.getValue());
+            resp.setMsg(e.getMessage());
+            resp.setMsg(e.getMessage());
+            log.error("Delete multiple Tổng hợp đề nghị cấp vốn DTQG lỗi ", e);
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+    @ApiOperation(value = "Export Tổng hợp đề nghị cấp vốn DTQG", response = List.class)
+    @PostMapping(value = "/export/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void exportListQdDcToExcel(HttpServletResponse response, @RequestBody KhDnThCapVonSearchRequest req) {
+
+        try {
+            service.exportToExcel(req, response);
+        } catch (Exception e) {
+            log.error("Error can not export", e);
+        }
     }
 }
