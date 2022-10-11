7a96869132ed9025d05caeaa3255ca8cd7e8a58b
othoa <othoa>
2022-10-11 16:09:26 +0700

    add Kh Von Phi Bo Nganh

---
+++ src/main/java/com/tcdt/qlnvkhoach/controller/qtVonPhiHang/KhVonPhiBoNganhController.java
@@ -0,0 +1,61 @@
+package com.tcdt.qlnvkhoach.controller.qtVonPhiHang;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tcdt.qlnvkhoach.controller.BaseController;
+import com.tcdt.qlnvkhoach.entities.phuongangia.KhPhuongAnGia;
+import com.tcdt.qlnvkhoach.request.DeleteRecordReq;
+import com.tcdt.qlnvkhoach.request.DeleteReq;
+import com.tcdt.qlnvkhoach.request.StatusReq;
+import com.tcdt.qlnvkhoach.request.phuongangia.KhLtPhuongAnGiaReq;
+import com.tcdt.qlnvkhoach.request.search.catalog.phuongangia.KhLtPhuongAnGiaSearchReq;
+import com.tcdt.qlnvkhoach.request.search.catalog.qtVonPhiHang.KhVonPhiBnSearchReq;
+import com.tcdt.qlnvkhoach.response.Resp;
+import com.tcdt.qlnvkhoach.service.phuongangia.KhLtPagService;
+import com.tcdt.qlnvkhoach.service.phuongangia.KhLtPhuongAnGiaService;
+import com.tcdt.qlnvkhoach.service.qtVonPhiHang.KhVonPhiBoNganhService;
+import com.tcdt.qlnvkhoach.util.Constants;
+import com.tcdt.qlnvkhoach.util.PathConstants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = PathConstants.VON_PHI_HANG)
+@Slf4j
+@Api(tags = "Kế hoạch, vốn phí hàng")
+public class KhVonPhiBoNganhController extends BaseController {
+
+    @Autowired
+    private KhVonPhiBoNganhService khVonPhiBoNganhService;
+
+
+    @ApiOperation(value = "Tra cứu đề xuất phương án giá", response = List.class)
+    @PostMapping(value=  PathConstants.VON_PHI_BO_NGANH + PathConstants.URL_TRA_CUU, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final ResponseEntity<Resp> search(@Valid @RequestBody KhVonPhiBnSearchReq objReq) {
+        Resp resp = new Resp();
+        try {
+            resp.setData(khVonPhiBoNganhService.searchPage(objReq));
+            resp.setStatusCode(Constants.RESP_SUCC);
+            resp.setMsg("Thành công");
+        } catch (Exception e) {
+            resp.setStatusCode(Constants.RESP_FAIL);
+            resp.setMsg(e.getMessage());
+            log.error(e.getMessage());
+        }
+        return ResponseEntity.ok(resp);
+    }
+
+}
